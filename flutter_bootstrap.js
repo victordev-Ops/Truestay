a6bf7a94ab5ed361d4acda2fb5358b25bd736031
victordev-Ops <victordev-Ops@users.noreply.github.com>
2025-08-19 18:03:00 +0000

    deploy: 11f49d17a03142c229fd2b8d059f75e633bfce0d

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2037539151"
+    serviceWorkerVersion: "4012543423"
   }
 });
