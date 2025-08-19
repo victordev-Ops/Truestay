f6e123c10b2e3f996168e344db9d157e1575191e
victordev-Ops <victordev-Ops@users.noreply.github.com>
2025-08-19 18:53:58 +0000

    deploy: b4bd66394e43348efe0241d9c6873970ae489a58

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "77e1134a37d9a63d605a07216bebe279",
+const RESOURCES = {"flutter_bootstrap.js": "83810bdceced60a059f332068cbdf0c3",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
